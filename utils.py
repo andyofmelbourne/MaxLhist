590883f581f504f30ae3ca09adb1042184372351
andyofmelbourne <andyofcfel@gmail.com>
2015-04-02 18:37:12 +0200

    add shifter

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def make_f_real(fhats, f_norm = 0.0):
     f is real and that sum_i f_i = f_norm. The fhats are 
     the complex Fourier space values of f from 1 to 
     N // 2 + 1 where N is the size of the real space array.
-
+    
     Parameters
     ----------
     fhats : 1d complex array
@@ -22,3 +22,27 @@ def make_f_real(fhats, f_norm = 0.0):
     fh = np.concatenate( ([f_norm], fhats) )
     f  = np.fft.irfft(fh)
     return f
+
+def shift_f_real(f, shift):
+    """
+    Apply the Fourier shift algorithm to f by shift pixels.
+    
+    Parameters
+    ----------
+    f : float array
+        Real space values of f.
+    shift : float
+        The value in pixels of the shift amount.
+            
+    Returns
+    -------
+    f_shift : 1d float array
+        the realspace representation of fhats given f_norm.
+    """
+    fh      = np.fft.rfft(f)
+    ramp    = np.exp(-2.0J * np.pi * shift * np.arange(float(fh.shape[0])) \
+              / float(f.shape[0]))
+    f_shift = np.fft.irfft(fh * ramp)
+    return f_shift
+
+
