b30d4c38fbcbf921d0b050904c19ad2ee5a07594
Andrew Morgan <andyofmelbourne@gmail.com>
2015-06-10 11:16:45 +0200

    more testing

--- test_X_update.py
+++ test_X_update.py
@@ -99,7 +99,7 @@ data = {
 
 # Retrieve
 #---------
-result = MaxLhist.refine([data2, data], iterations=1, processes = processes)
+result = MaxLhist.refine([data], iterations=1, processes = processes)
 
 print 'fidelity counts :' , np.sum((counts - result.result['run']['counts'])**2)/np.sum(counts**2)
 print 'fidelity gain   :' , np.sum((gs - result.result['run']['gain'])**2)/np.sum(gs**2)
