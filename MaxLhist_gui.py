506d4b736b3ad6431f98a88f6599ee026e461632
Andrew Morgan <andyofmelbourne@gmail.com>
2015-06-26 17:57:08 +0200

    job runner should be working now...

--- MaxLhist_gui.py
+++ MaxLhist_gui.py
@@ -11,7 +11,6 @@ import ConfigParser
 Ui_MainWindow, QMainWindow = PyQt4.uic.loadUiType('gui.ui')
 
 
-
 def config_writer(h5dir, h5fnam, init=True):
     """
     Write a config file for MaxL_hist and put it into h5dir
@@ -23,19 +22,58 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         
+        self.process_options = None
+        
         # Set up the user interface from Designer.
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui.statusbar.showMessage('Load a process.config file...')
+        self.ui.actionLoad_process_file.triggered.connect(self.loadProcess)
+        self.ui.actionRunMaxL.triggered.connect(self.runMaxL)
+
+    def runMaxL(self):
+        if self.process_options is None :
+            self.ui.statusbar.showMessage('You need to load some histograms!')
+            return
         
-        self.ui.actionLoad_process_file.triggered.connect(self.selectFile)
+        # get the selected histogram 
+        n = self.ui.tableWidget.currentRow()
+        print 'current histdir and name:', self.hist_dirs[n], self.hist_fnams[n]
 
+        # get the selected algorithm 
+        fnam = PyQt4.QtGui.QFileDialog.getOpenFileName(\
+                directory = os.path.dirname(self.process_file),\
+                filter = "Python files (*.py)")
+        self.alg_file  = str(fnam)
+        self.ui.statusbar.showMessage('loaded algorithm:' + self.alg_file)
+        
+        # export MaxLhist_MPI
+        from subprocess import call
+        print os.path.abspath('.')
+        print 'export PYTHONPATH+=:' + os.path.abspath('./')+'/'
+        call(['export PYTHONPATH+=:' + os.path.abspath('./')+'/'], shell=True)
+        
+        # run the script
+        runstr = 'mpirun -n 4 python ' + self.alg_file + ' ' + self.hist_dirs[n] +'/'+ self.hist_fnams[n] +' '+ self.process_options['h5_data_path'] + ' -o ' + self.hist_dirs[n] + '/'
+        
+        print runstr
+        #import subprocess
+        #p = subprocess.Popen([runstr], stdout=subprocess.PIPE,\
+        #                               stderr=subprocess.PIPE,\
+        #                               shell=True)
+        #out, err = p.communicate()
+        #print out
+        
+        call([runstr], shell=True)
+        print 'done...'
 
-    def selectFile(self):
+    def loadProcess(self):
+        fnam = './'
         try :
             f = open('.gui', 'r')
-            fnam = f.read(self.process_file)
-        except :
-            fnam = '.'
+            fnam = f.read()
+            print '.gui file:', fnam
         finally :
             f.close()
         
@@ -45,8 +83,9 @@ class MainWindow(QMainWindow):
         self.process_file    = str(fnam)
         
         # save the last selection for the next time this program is run
-        with open('.gui', 'w') as f:
-            f.write(self.process_file)
+        if self.process_file is not None and self.process_file != '':
+            with open('.gui', 'w') as f:
+                f.write(self.process_file)
         
         self.process_options = self.load_process_file(self.process_file)
         self.load_hist_table(self.process_options, self.ui.tableWidget)
